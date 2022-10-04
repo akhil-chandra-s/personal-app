0d1d8114e1707094edae4ab5fe1a4c3e97721aa1
Akhil Chandra Shanivendra <110700785+akhil-chandra-s@users.noreply.github.com>
2022-10-03 21:48:42 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -161,6 +161,11 @@ st.markdown('''
 ## Projects
 ''')
 txt3('Credit_Card_Fraud_Detection', 'https://github.com/akhil-chandra-s/Credit_Card_Fraud_Detection')
+st.markdown('''
+- Predict fraudulent credit card transactions so that the customers of credit card companies are not charged for items that they did not purchase.
+- Used libraries like Numpy, Pandas, Scikit-Learn, Matplotlib, pycaret for comparing all models with target model.
+- Random Forest was the first model used to preict the outcome, then I compared it with all the models using the pycaret library.
+''')
 txt3('Customer_Churn_Telecom', 'https://github.com/akhil-chandra-s/Loan_Prediction')
 txt3('Insurance_Claim_Prediction', 'https://github.com/akhil-chandra-s/Insurance_Claim_Prediction')
 txt3('Loan_Prediction', 'https://github.com/akhil-chandra-s/Loan_Prediction')
